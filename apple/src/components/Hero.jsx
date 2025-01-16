e92096fc83853e216bc5d775b9ceb52560c525c7
neuervinch <npandian515@gmail.com>
2025-01-16 23:08:41 +0530

    First touch

---
+++ apple/src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Hero = () => {
+    
+  return (
+    <section>
+        <div>
+            <p>Android is better Than iOS</p>
+            <div>
+                <video>
+                    <source/>
+                </video>
+            </div>
+        </div>
+
+        <div>
+            <a>Buy</a>
+            <p> Never Sell your Kidneys</p>
+        </div>
+
+    </section>
+  )
+}
+
+export default Hero
